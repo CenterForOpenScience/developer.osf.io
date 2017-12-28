bca93a8544b7ce086f032d8fd36840cbfa8c550d
Casey Rollins <case.rollins@gmail.com>
2017-12-28 13:46:44 -0500

    Add cachbusting and livereload gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var run = require('gulp-run');
+var cachebust = require('gulp-cache-bust');
 
 gulp.task('validate', function() {
   return run('swagger validate swagger-spec/swagger.yaml').exec()
@@ -12,13 +13,25 @@ gulp.task('bundle', ['validate'], function() {
   return run('swagger bundle -r -o swagger.json swagger-spec/swagger.yaml').exec();
 });
 
-gulp.task('connect', ['bundle'], function() {
+gulp.task('connect', ['cachebust'], function() {
   return run('http-server -p 8080').exec();
+});
+
+gulp.task('livereload', ['cachebust'], function() {
+  return run('watch-http-server -p 8080').exec();
+});
+
+gulp.task('cachebust', ['bundle'], function() {
+  gulp
+    .src('./')
+    .pipe(cachebust({type:'timestamp'}))
+    .pipe(gulp.dest('./'))
 });
 
 function log(error) {
   console.error(error.toString && error.toString());
 }
 
-gulp.task('default', ['bundle']);
+gulp.task('default', ['cachebust']);
+gulp.task('watch', ['livereload']);
 gulp.task('serve', ['connect']);
